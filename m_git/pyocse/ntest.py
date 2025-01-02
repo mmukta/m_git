fix the computation of get_objective

m_git/pyocse/ntest.py
@@ -9,7 +9,7 @@ def obj_function(para_values, params, para0, terms, ref_dics, e_offset):
     Objective function for PSOGAOptimizer.
 
     Args:
-        para_values: Array of parameter values to evaluate.
+        para_values: 1D-Array of parameter values to evaluate.
         params: parameter instance
         para0: Array of all FF parameter as the base
         terms: list of FF terms to optimize
@@ -42,8 +42,10 @@ def obj_function(para_values, params, para0, terms, ref_dics, e_offset):
         ref_dics=ref_dics,
         e_offset=e_offset,
         lmp_in=lmp_in,
-        obj="MSE"
+        #obj="MSE"
+        obj="R2"
     )
+
     return objective_score
 
 
@@ -51,8 +53,8 @@ np.random.seed(7)
 
 params = ForceFieldParameters(
     smiles=['CC(=O)OC1=CC=CC=C1C(=O)O'],
-    f_coef=1,
-    s_coef=1,
+    f_coef=1, #0.1,
+    s_coef=1, #0,
     e_coef=1,
     style='openff',
     ref_evaluator='mace',
@@ -68,11 +70,14 @@ os.chdir("ASP2")
 t0 = time()
 e_offset, params_opt = params.optimize_offset(ref_dics, p0)
 params.update_ff_parameters(params_opt)
+errs = params.plot_ff_results("performance_opt_pso_0.png", ref_dics, [params_opt])
 print("MSE objective", params.get_objective(ref_dics, e_offset, obj="MSE"))
+print("R2 objective", params.get_objective(ref_dics, e_offset, obj="R2"))
+#import sys; sys.exit()
 
 # Stepwise optimization loop
 for data in [
-    (["bond", "angle", "proper", "vdW"], 100),
+    (["bond", "angle", "proper", "vdW"], 10), #00),
 ]:
     (terms, steps) = data
 
@@ -86,7 +91,7 @@ for data in [
         obj_args=(params, params_opt, terms, ref_dics, e_offset),
         bounds=bounds,
         seed=vals.reshape((1, len(vals))),
-        num_particles=100,
+        num_particles=10, #0,
         dimensions=len(bounds),
         inertia=0.5,
         cognitive=0.2,
@@ -105,16 +110,19 @@ for data in [
     count = 0
     for term in terms:
         N = getattr(params, 'N_'+term)
-        sub_values.append(best_position[count:count+N])
+        sub_values.append(best_position[count: count+N])
         count += N
+
     params_opt = params.set_sub_parameters(sub_values, terms, params_opt)
-    _, params_opt = params.optimize_offset(ref_dics, params_opt)
+    e_offset, params_opt = params.optimize_offset(ref_dics, params_opt)
+    params.update_ff_parameters(params_opt)
+    print("e_offset", e_offset)
 
     t = (time() - t0) / 60
     print(f"\nStepwise optimization for terms {terms} completed in {t:.2f} minutes.")
     print(f"Best Score: {best_score:.4f}")
 
 # Final evaluation and saving results
-errs = params.plot_ff_results("performance_opt_pso_102.png", ref_dics, [params_opt])
+errs = params.plot_ff_results("performance_opt_pso_1.png", ref_dics, [params_opt])
 params.export_parameters("parameters_opt_pso.xml", params_opt, errs[0])
 print("Optimization completed successfully.")

m_git/pyocse/psoga_optimizer.py
@@ -42,25 +42,27 @@ class PSOGAOptimizer:
         self.upper_bounds = np.array([b[1] for b in bounds])
 
         # Initialize particles and Rescale to (0, 1)
-        self.positions = np.random.uniform(self.lower_bounds, self.upper_bounds, (num_particles, dimensions))
+        self.positions = np.random.uniform(self.lower_bounds, 
+                                           self.upper_bounds, 
+                                           (num_particles, dimensions))
         self.positions = (self.positions - self.lower_bounds) / (self.upper_bounds - self.lower_bounds)
         self.velocities = 0.1 * np.random.uniform(-1, 1, (num_particles, dimensions))
 
         if seed is not None:
             self.set_seeds(seed)
 
+
+        # Init: evaluate the score for each particle
         self.personal_best_positions = np.copy(self.positions)
-        # evaluate the score for each particle
         self.personal_best_scores = np.zeros(len(self.positions))
         for i, p in enumerate(self.positions):
             p_actual = self.rescale(p)
             score = self.safe_evaluate(p_actual)
             self.personal_best_scores[i] = score
             print(f"{i} score: {score} max_V: {np.abs(self.velocities[i]).max()}")
-            #print(self.positions[i])
-        #self.personal_best_scores = np.array([self.safe_evaluate(p) for p in self.positions])
 
-        self.global_best_position = self.personal_best_positions[np.argmin(self.personal_best_scores)]
+        min_idx = np.argmin(self.personal_best_scores)
+        self.global_best_position = self.personal_best_positions[min_idx]
         self.global_best_score = np.min(self.personal_best_scores)
 
     def set_seeds(self, seed):
@@ -180,4 +182,5 @@ class PSOGAOptimizer:
                 #print(f"Global Best Position: {self.global_best_position}")
                 #print(f"Velocities:\n{self.velocities}")
                 #print(f"Positions:\n{self.positions}")
-        return self.global_best_position, self.global_best_score
+        best_position = self.rescale(self.global_best_position)
+        return best_position, self.global_best_score